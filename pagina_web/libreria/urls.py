8f146e8bbd85f1a8cf3ffb2e2c54e3e6539b1e1c
raurojas <rau.rojas@duocuc.cl>
2024-04-21 22:44:28 -0400

    Raul
    Cambio de nombre inicio a index

--- pagina_web/libreria/urls.py
+++ pagina_web/libreria/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import inicio, nosotros, menus, registro
+from .views import index, nosotros, menus, registro
 
 
 urlpatterns = [
-    path('', inicio, name = 'inicio'),
+    path('', index, name = 'index'),
     path('nosotros', nosotros, name = 'nosotros'),
     path('menus', menus, name = 'menus'),
     path('registro', registro , name = 'registro')
--- pagina_web/libreria/views.py
+++ pagina_web/libreria/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 
-def inicio(request):
-    return render (request, 'paginas/inicio.html')
+def index(request):
+    return render (request, 'paginas/index.html')
 
 def nosotros(request):
     return render (request, 'paginas/nosotros.html')
