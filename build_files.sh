889cd7f3885741b3509f36bf4f5c18f6023406fc
Mursito1 <deltamur@hotmail.com>
2024-04-23 16:45:57 -0400

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -2,3 +2,6 @@
  python3 -m pip install -r requirements.txt
  python3 manage.py collectstatic --noinput --clear
  echo "BUILD END"
+
+ mkdir -p staticfiles_build
+ mv generated_files/* staticfiles_build/
